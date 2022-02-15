2f5d319c38fec5d3f5b8214b5e29fbbb254ffbda
mjfox33 <mjfox33@gmail.com>
2022-02-15 16:42:19 -0500

    switched to dynamic programming for speed

--- code_96_unique_binary_search_trees.py
+++ code_96_unique_binary_search_trees.py
@@ -4,9 +4,10 @@ class Solution:
             return None
         if n < 2:
             return 1
-        result = 0
-        for i in range(n):
-            result += self.numTrees(i) * self.numTrees(n-i-1)
-        
-        return result
-
+        result = [0]*(n+1)
+        result[0] = 1
+        result[1] = 1
+        for i in range(2, n+1):
+            for j in range(i):
+                result[i] +=  result[j] * result[i-j-1]
+        return result[n]
